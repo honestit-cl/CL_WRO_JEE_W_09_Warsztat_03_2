850356f20a5e5a213512c50dde6affe118537b0b
honestit <michal.kupisinski@gmail.com>
2020-07-26 20:50:51 +0200

    Konfiguracja połączenia do db i testowy servlet

---
+++ src/main/java/pl/coderslab/web/TestServlet.java
@@ -0,0 +1,25 @@
+package pl.coderslab.web;
+
+import pl.coderslab.utils.DbUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Connection connection = DbUtil.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
